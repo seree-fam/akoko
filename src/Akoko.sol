be859f700927e7ded946058e1dbac8409d4f6e27
lancenonce <lancendavis47@gmail.com>
2024-07-11 16:36:02 +0000

    add uuid

--- src/Akoko.sol
+++ src/Akoko.sol
@@ -14,8 +14,10 @@ contract Akoko {
 
     enum Status { Paid, Unpaid }
 
+    // todo: add a uuid string
     struct Order {
-        uint256 id;
+        string uuid;
+        uint256 id; // hash of the uuid
         Status status;
         uint256 amount;
         string recipient; // the recipient is typically a phone number
@@ -24,7 +26,7 @@ contract Akoko {
     mapping(uint256 => Order) public orders;
     mapping(uint256 => uint256) public escrowBalances;
 
-    event OrderPlaced(uint256 indexed id, Status status, uint256 amount, string recipient);
+    event OrderPlaced(string indexed uuid, uint256 indexed id, Status status, uint256 amount, string recipient);
     event Payout(uint256 indexed id, uint256 amount);
 
     modifier onlyOwner() {
@@ -37,13 +39,14 @@ contract Akoko {
         usdcToken = IERC20(_usdcTokenAddress);
     }
 
-    function placeOrder(uint256 _id, string memory _recipient, uint256 _usdcAmount) public {
+    function placeOrder(string memory _uuid, uint256 _id, string memory _recipient, uint256 _usdcAmount) public {
         require(_usdcAmount > 0, "USDC amount must be greater than 0");
 
         uint256 allowance = usdcToken.allowance(msg.sender, address(this));
         require(allowance >= _usdcAmount, "Check the token allowance");
 
         Order memory newOrder = Order({
+            uuid: _uuid,
             id: _id,
             status: Status.Unpaid,
             amount: _usdcAmount,
@@ -56,7 +59,7 @@ contract Akoko {
         bool success = usdcToken.transferFrom(msg.sender, address(this), _usdcAmount);
         require(success, "USDC transfer failed");
 
-        emit OrderPlaced(_id, Status.Unpaid, _usdcAmount, _recipient);
+        emit OrderPlaced(_uuid, _id, Status.Unpaid, _usdcAmount, _recipient);
     }
 
     function payout(uint256 _id) public onlyOwner {
