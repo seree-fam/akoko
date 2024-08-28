efb7356855a4f445ac14e5425016deea1fc64cbc
lancenonce <lancendavis47@gmail.com>
2024-08-28 13:46:50 +0300

    payout signature, usdc/t support

--- src/Akoko.sol
+++ src/Akoko.sol
@@ -11,14 +11,18 @@ interface IERC20 {
 
 contract Akoko {
     address public owner;
+    address public usdcContract;
+    address public usdtContract;
     IERC20 public usdcToken;
     IERC20 public usdtToken;
 
     enum Status { Paid, Unpaid }
+    enum Token { Ether, Usdc, Usdt }
 
     struct Order {
         bytes32 uuid;
         Status status; 
+        Token token;
     }
 
     mapping(uint256 => Order) public orders;
@@ -30,6 +34,71 @@ contract Akoko {
     modifier onlyOwner() {
         require(msg.sender == owner, "Caller is NOT the owner");
         _;
+    }
+
+    constructor () {
+        owner = msg.sender;
+    }
+
+    function placeOrderEth(bytes32 _uuid) public payable {
+        require(msg.value > 0, "ETH amount must be greater than 0");
+
+        Order memory newOrder = Order({
+            uuid: _uuid,
+            status: Status.Unpaid,
+            token: Token.Ether
+        });
+
+        orders[uint256(_uuid)] = newOrder;
+        escrowBalances[uint256(_uuid)] = msg.value;
+
+        emit OrderPlaced(_uuid);
+    }
+
+    function placeOrderUSDC(bytes32 _uuid, uint256 _usdcAmount) public {
+        require(_usdcAmount > 0, "USDC amount must be greater than 0");
+
+        uint256 allowance = usdcToken.allowance(msg.sender, address(this));
+        require(allowance >= _usdcAmount, "Please check the token allowance");
+
+        Order memory newOrder = Order({
+            uuid: _uuid,
+            status: Status.Unpaid,
+            token: Token.Usdc
+        });
+
+        orders[uint256(_uuid)] = newOrder;
+        escrowBalances[uint256(_uuid)] = _usdcAmount;
+
+        bool success = usdcToken.transferFrom(msg.sender, address(this), _usdcAmount);
+        require(success, "USDC transfer failed");
+
+        emit OrderPlaced(_uuid);
+    }
+
+    function placeOrderUSDT(bytes32 _uuid, uint256 _usdtAmount) public {
+        require(_usdtAmount > 0, "USDT amount must be greater than 0");
+
+        uint256 allowance = usdtToken.allowance(msg.sender, address(this));
+        require(allowance >= _usdtAmount, "Please check the token allowance");
+
+        Order memory newOrder = Order({
+            uuid: _uuid,
+            status: Status.Unpaid,
+            token: Token.Usdt
+        });
+
+        orders[uint256(_uuid)] = newOrder;
+        escrowBalances[uint256(_uuid)] = _usdtAmount;
+
+        bool success = usdtToken.transferFrom(msg.sender, address(this), _usdtAmount);
+        require(success, "USDT transfer failed");
+
+        emit OrderPlaced(_uuid);
+    }
+
+    function payout(bytes32 _uuid, bytes32 message_hash, uint256 r, uint256 s, uint256 x, uint256 y) public onlyOwner {
+        
     }
 
     receive() external payable {}
